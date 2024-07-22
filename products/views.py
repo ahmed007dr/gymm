838a67597710388a44405880899b7257baa5b325
ahmed007dr <ahmed007eg@gmail.com>
2024-07-23 00:53:57 +0300

    details

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-# Create your views here.
-from django.views.generic import ListView,DetailView
-
-from .models import Gym , GymImages , Brand
+from django.views.generic import ListView, DetailView
+from .models import Gym, GymImages, Brand
 
 class GymList(ListView):
     model = Gym
 
 class GymDetails(DetailView):
     model = Gym
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['images'] = GymImages.objects.filter(product=self.get_object())
+        return context
