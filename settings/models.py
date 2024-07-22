ce6d69f9b8e4ba4a8f798a92d183329ba66cd6f4
ahmed007dr <ahmed007eg@gmail.com>
2024-07-23 02:04:57 +0300

    create settings app models

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Settings(models.Model):
+    name=models.CharField(max_length=100)
+    logo=models.ImageField(upload_to='settings')
+    logo_banner = models.ImageField(upload_to='settings')
+    text_banner = models.TextField(max_length=500)
+    text_banner2 = models.TextField(max_length=500)
+    subtitle=models.TextField(max_length=500)
+    call_us=models.CharField(max_length=25)
+    emails_us=models.TextField(max_length=25)
+    emails=models.TextField(max_length=50)
+    phone=models.TextField(max_length=50)
+    address=models.TextField(max_length=100)
+    android_app=models.URLField(null=True,blank=True)
+    ios_app=models.URLField(null=True,blank=True)
+    facebook=models.URLField(null=True,blank=True)
+    youtube=models.URLField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+    
