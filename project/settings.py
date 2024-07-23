a507ad7770c46ca9efd6fcb3a0bd7673ddcceb0e
ahmed007dr <ahmed007eg@gmail.com>
2024-07-24 02:15:41 +0300

    contexxt pro

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'settings.context_processors.get_settings',  # Add this line
+                'settings.settings_context_processor.get_settings',  # Add this line
             ],
         },
     },
