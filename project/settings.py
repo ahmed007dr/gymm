1d2814316fd71eaad46e1a071a19c00ca3f7e303
ahmed007dr <ahmed007eg@gmail.com>
2024-07-23 19:06:41 +0300

    host

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-*r@f7k)8q3sk#kq3%%r-nh!qd!e5fejs(qo@qkv3y=%!v^c-!g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ahmed-shop.onrender.com']
 
 
 # Application definition
