494eb52bc0729b2dd74829b56c54237175d1d3f5
ahmed007dr <ahmed007eg@gmail.com>
2024-07-25 23:35:04 +0300

    static file

--- project/settings.py
+++ project/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # This is where 'collectstatic' will collect static files
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
